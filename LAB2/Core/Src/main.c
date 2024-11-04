ea3bfd9fb51e7ee3227310618ea8f97fdb541036
NGUYEN NGOC PHU <phunguyen30082004@gmail.com>
2024-11-05 02:53:31 +0700

    lab2_ex3

--- LAB2/Core/Src/main.c
+++ LAB2/Core/Src/main.c
@@ -251,6 +251,47 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+ const int MAX_LED = 4;
+ int index_led = 0;
+ int led_buffer [4] = {1 , 2 , 3 , 4};
+ void update7SEG ( int index ) {
+     switch ( index ) {
+       case 0:
+         // Display the first 7 SEG with led_buffer [0]
+    	   HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin, GPIO_PIN_SET);
+    	   HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin, GPIO_PIN_SET);
+    	   HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin, GPIO_PIN_SET);
+    	   display7SEG(led_buffer[0]);
+    	   HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin, GPIO_PIN_RESET);
+         break ;
+       case 1:
+         // Display the second 7 SEG with led_buffer [1]
+    	   HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin, GPIO_PIN_SET);
+    	   HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin, GPIO_PIN_SET);
+    	   HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin, GPIO_PIN_SET);
+      	   display7SEG(led_buffer[1]);
+       	   HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin, GPIO_PIN_RESET);
+         break ;
+       case 2:
+         // Display the third 7 SEG with led_buffer [2]
+    	   HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin, GPIO_PIN_SET);
+       	   HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin, GPIO_PIN_SET);
+       	   HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin, GPIO_PIN_SET);
+       	   display7SEG(led_buffer[2]);
+       	   HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin, GPIO_PIN_RESET);
+         break ;
+       case 3:
+         // Display the forth 7 SEG with led_buffer [3]
+    	   HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin, GPIO_PIN_SET);
+       	   HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin, GPIO_PIN_SET);
+       	   HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin, GPIO_PIN_SET);
+       	   display7SEG(led_buffer[3]);
+       	   HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin, GPIO_PIN_RESET);
+         break ;
+       default :
+         break ;
+      }
+ }
   int counter = 200;
   int cnt = 100;
   void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef * htim )
@@ -264,34 +305,15 @@ static void MX_GPIO_Init(void)
     if( counter <= 0) {
         counter = 200;
     }
-    if(counter >= 150){
-    	HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin, GPIO_PIN_SET);
-    	HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin, GPIO_PIN_SET);
-    	HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin, GPIO_PIN_SET);
-    	display7SEG(1);
-    	HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin, GPIO_PIN_RESET);
+    if(index_led > 3){
+    	index_led = 0;
     }
-    if(counter >=100 && counter<150){
-    	HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin, GPIO_PIN_SET);
-    	HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin, GPIO_PIN_SET);
-    	HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin, GPIO_PIN_SET);
-    	display7SEG(2);
-    	HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin, GPIO_PIN_RESET);
+    update7SEG (index_led);
+    if(counter%50 == 0){
+    	index_led++;
     }
-    if(counter >= 50 && counter < 100){
-    	HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin, GPIO_PIN_SET);
-    	HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin, GPIO_PIN_SET);
-    	HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin, GPIO_PIN_SET);
-    	display7SEG(3);
-    	HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin, GPIO_PIN_RESET);
-    }
-    if(counter < 50){
-    	HAL_GPIO_WritePin ( EN1_GPIO_Port , EN1_Pin, GPIO_PIN_SET);
-    	HAL_GPIO_WritePin ( EN2_GPIO_Port , EN2_Pin, GPIO_PIN_SET);
-    	HAL_GPIO_WritePin ( EN0_GPIO_Port , EN0_Pin, GPIO_PIN_SET);
-    	display7SEG(0);
-    	HAL_GPIO_WritePin ( EN3_GPIO_Port , EN3_Pin, GPIO_PIN_RESET);
-       }
+
+
  }
 /* USER CODE END 4 */
 
